93f1534243abb47d9099872fa553b3ff68fac437
Byron Jenkins <codeherk@gmail.com>
2023-04-13 05:19:24 -0400

    Implement MySQL connection #3 * Add docker-compose.yml with mysql container definition * Add init.sql for creating and populating table * Add data.draw.io for Data Model diagram

--- main.go
+++ main.go
@@ -1,32 +1,75 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-	"time"
+	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
-// TODO: Define Task Struct
-type task struct {
+// Database object for handlers to use
+var db *sql.DB
+
+type Task struct {
 	Description      string `json:"description"`
 	CreatedTimestamp string `json:"createdTimestamp"`
 }
 
-// TODO: Define tasks variable globally
-var tasks = []task{
-	{Description: "Get 10k followers", CreatedTimestamp: time.Now().Format(time.RFC3339)},
-	{Description: "Get paid from content", CreatedTimestamp: time.Now().Format(time.RFC3339)},
+// TODO: Implement API Error Handling
+// type ErrorResponse struct {
+// 	Error   string `json:"error"`
+// 	Message string `json:"message"`
+// 	Detail  string `json:"detail"`
+// }
+
+func getHealth(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 }
 
-// TODO: Define getTasks Handler
 func getTasks(w http.ResponseWriter, r *http.Request) {
+	log.Println("attempting query to Task table")
+
+	// Query Task Table
+	rows, err := db.Query("SELECT description, created_timestamp FROM Tasks")
+	if err != nil {
+		log.Printf("Error querying Task table: %s\n", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	defer rows.Close()
+
+	var tasks []Task
+	for rows.Next() {
+		var task Task
+		err := rows.Scan(&task.Description, &task.CreatedTimestamp)
+		if err != nil {
+			log.Printf("Error converting rows : %s\n", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		tasks = append(tasks, task)
+	}
+
+	// if err := rows.Err(); err != nil {
+	// 	panic(err.Error())
+	// }
+
+	for _, task := range tasks {
+		// fmt.Printf("Task ID: %d\n", task.ID)
+		fmt.Printf("Description: %s\n", task.Description)
+		fmt.Printf("Created Timestamp: %s\n", task.CreatedTimestamp)
+	}
+
 	// Marshal tasks into json
 	resp, err := json.Marshal(tasks)
+
 	if err != nil {
-		log.Fatalf("Error marshalling tasks to json: %s", err.Error())
+		log.Printf("Error marshalling tasks to json: %s\n", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
 	}
 
 	// Return response
@@ -36,11 +79,37 @@ func getTasks(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := mux.NewRouter()
 
+	// Get MYSQL variables
+	username := os.Getenv("MYSQL_USER")
+	password := os.Getenv("MYSQL_PASSWORD")
+	host := os.Getenv("MYSQL_HOST")
+	port := os.Getenv("MYSQL_PORT")
+	database := os.Getenv("MYSQL_DATABASE")
+
+	log.Println("Attempting to open MySQL connection")
+
+	// Establish DB connection. Format: USER:PASSWORD@tcp(HOST:PORT)/DATABASE
+	var err error
+	db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, host, port, database))
+
+	if err != nil {
+		log.Fatalf("Error opening MySQL connection: %s", err.Error())
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		log.Fatalf("Error pinging MySQL database: %s", err.Error())
+	} else {
+		log.Println("Successful ping")
+	}
+
 	// TODO: Register new route
+	r.HandleFunc("/health", getHealth).Methods("GET")
 	r.HandleFunc("/tasks", getTasks).Methods("GET")
 
-	log.Print("Now serving on port 8090")
-	err := http.ListenAndServe(":8090", r)
+	log.Println("Now serving on port 8090")
+	err = http.ListenAndServe(":8090", r)
 
 	if err != nil {
 		log.Fatal(err)
